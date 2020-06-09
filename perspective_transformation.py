e3d42e75fef063ebc0a1ec039878a59f78978237
Dynaa <ma.mittet@gmail.com>
2020-06-09 23:18:17 +0200

    feat : add transformation matrix to output in order to reproject lane on original image

--- perspective_transformation.py
+++ perspective_transformation.py
@@ -11,27 +11,21 @@ def unwarp(img):
 
 	
 	# Define 4 sources points : picked manually 
-	src = np.float32([[620,450],[715,450],[295,680],[1088,680]])
+	src = np.float32([[490,480],[800,480],[1250,720],[40,720]])
 
 	# Define 4 destination points
-	dst = np.float32([(450,0),(img.shape[1]-450,0),(450,img.shape[0]),(img.shape[1]-450,img.shape[0])])
+	dst = np.float32([[0,0],[1280,0],[1250,720],[40,720]])
 
 	M = cv2.getPerspectiveTransform(src,dst)
 	warped = cv2.warpPerspective(img, M, (img.shape[1], img.shape[0]), flags=cv2.INTER_LINEAR)
-	return warped
+	return warped, M
 
 
 
 if __name__ == '__main__':
 	image = mpimg.imread('test_images/test1.jpg')
-	result = pipeline(image)
+	result = unwarp(image)
 
-
-
-	#cv2.line(warped, tuple(dst[0]), tuple(dst[1]), [255,0,0], 2)
-	#cv2.line(warped, tuple(dst[1]), tuple(dst[2]), [255,0,0], 2)
-	#cv2.line(warped, tuple(dst[2]), tuple(dst[3]), [255,0,0], 2)
-	#cv2.line(warped, tuple(dst[3]), tuple(dst[0]), [255,0,0], 2)
 
 	# Plot the result
 	f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
